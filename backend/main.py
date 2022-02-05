b3a63cb1c8a2f23c4646996755ab9cadc28290ee
sumanth143-chevuru <sumanthchevuru94@gmail.com>
2022-02-05 17:03:06 +0530

    created webapp folder and registartion

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,20 @@ from core.config import settings
 from db.session import engine
 from db.base import Base
 from apis.base import api_router
+from webapps.base import api_router as web_app_router
+
+
 
 def include_router(app):
 	app.include_router(api_router)
-
+	app.include_router(web_app_router)
+	
 
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-def create_tables():           #new
+def create_tables():
 	Base.metadata.create_all(bind=engine)
 
 
@@ -21,7 +25,7 @@ def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
-	create_tables()       #new
+	create_tables()
 	return app
 
 app = start_application()
---
+++ backend/webapps/base.py
@@ -0,0 +1,8 @@
+from webapps.jobs import route_jobs
+from webapps.users import route_users
+from fastapi import APIRouter
+
+
+api_router = APIRouter()
+api_router.include_router(route_jobs.router, prefix="", tags=["job-webapp"])
+api_router.include_router(route_users.router, prefix="", tags=["users-webapp"])
